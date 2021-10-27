392f7d78be5c2999f78cf884833f38e6bc59dc35
larsonmpdx <larsonm.pdx@gmail.com>
2021-10-26 20:53:00 -0700

    more

--- harvest-chart-server/src/json_format/main.rs
+++ harvest-chart-server/src/json_format/main.rs
@@ -6,6 +6,7 @@ use json5format::*;
 use std::fs;
 use std::io;
 use std::io::{Read, Write};
+use walkdir::WalkDir;
 
 fn write_to_file(filename: &str, bytes: &[u8]) -> Result<(), io::Error> {
     fs::OpenOptions::new()
@@ -17,39 +18,54 @@ fn write_to_file(filename: &str, bytes: &[u8]) -> Result<(), io::Error> {
 }
 
 fn main() -> Result<(), std::io::Error> {
-    let database_dir = import_db::get_database_dir().unwrap();
+    let options = FormatOptions {
+        indent_by: 4,
+        trailing_commas: true,
+        collapse_containers_of_one: true,
+        sort_array_items: false,
+        ..Default::default()
+    };
+    let format = Json5Format::with_options(options).unwrap();
 
+    let database_dir = import_db::get_database_dir().unwrap();
     println!("found database dir: {:?}", database_dir);
 
     let file_paths = fs::read_dir(database_dir.join("plants")).unwrap();
-
     for file_path in file_paths {
         let path_ = file_path.unwrap().path();
-
-        let options = FormatOptions {
-            indent_by: 4,
-            trailing_commas: true,
-            collapse_containers_of_one: true,
-            sort_array_items: false,
-            ..Default::default()
-        };
-        let format = Json5Format::with_options(options).unwrap();
 
         if fs::metadata(path_.clone()).unwrap().is_file()
             && path_.extension().unwrap().to_str().unwrap() == "json5"
         {
-            println!("loading: {}", path_.display());
-
-            let filename = path_.clone().into_os_string().to_string_lossy().to_string();
-
-            let mut buffer = String::new();
-            fs::File::open(&path_)?.read_to_string(&mut buffer)?;
-            let parsed_document =
-                ParsedDocument::from_string(buffer, Some(filename.clone())).unwrap();
-            let bytes = format.to_utf8(&parsed_document).unwrap();
-            write_to_file(&filename, &bytes)?;
+            run_format(path_, &format)?;
         }
     }
 
+    for entry in WalkDir::new(database_dir.join("references"))
+        .max_depth(5)
+        .into_iter()
+        .filter_map(|e| e.ok())
+    {
+        let path_ = entry.path();
+
+        if fs::metadata(path_).unwrap().is_file()
+            && path_.extension().unwrap().to_str().unwrap() == "json5"
+        {
+            run_format(path_.to_path_buf(), &format)?;
+        }
+    }
+
+    Ok(())
+}
+
+fn run_format(path_: std::path::PathBuf, format: &Json5Format) -> Result<(), io::Error> {
+    println!("formatting: {}", path_.display());
+    let filename = path_.clone().into_os_string().to_string_lossy().to_string();
+    let mut buffer = String::new();
+    fs::File::open(&path_)?.read_to_string(&mut buffer)?;
+    let parsed_document =
+        ParsedDocument::from_string(buffer, Some(filename.clone())).unwrap();
+    let bytes = format.to_utf8(&parsed_document).unwrap();
+    write_to_file(&filename, &bytes)?;
     Ok(())
 }
