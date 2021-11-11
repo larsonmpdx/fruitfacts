54ccc29e14edb70129bcb242f0874bf04e066e11
larsonmpdx <larsonm.pdx@gmail.com>
2021-11-11 10:43:21 -0800

    wwfrf recommendation page

--- harvest-chart-server/src/import_db/test.rs
+++ harvest-chart-server/src/import_db/test.rs
@@ -388,11 +388,8 @@ fn test_database_loading() {
     assert_ge!(items_loaded.base_plants_found, 234);
     assert_ge!(items_loaded.base_types_found, 47);
     assert_ge!(items_loaded.reference_items.reference_locations_found, 47);
-    assert_ge!(
-        items_loaded.base_plants_found + items_loaded.reference_items.reference_base_plants_added,
-        2591
-    );
-    assert_ge!(items_loaded.reference_items.reference_plants_added, 4538);
+    assert_ge!(items_loaded.reference_items.reference_base_plants_added, 2372);
+    assert_ge!(items_loaded.reference_items.reference_plants_added, 4644);
 }
 
 #[test]
