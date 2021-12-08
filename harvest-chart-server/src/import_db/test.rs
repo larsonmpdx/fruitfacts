6f84a5187e87de0c11ecf0575287415e19e1a8f3
larsonmpdx <larsonm.pdx@gmail.com>
2021-12-08 15:20:18 -0800

    more

--- harvest-chart-server/src/import_db/test.rs
+++ harvest-chart-server/src/import_db/test.rs
@@ -493,14 +493,14 @@ fn test_database_loading() {
     println!("loaded: {:#?}", items_loaded);
 
     // update these every so often so we can check that a change doesn't cause fewer items than we expect
-    assert_ge!(items_loaded.base_plants_found, 207);
-    assert_ge!(items_loaded.base_types_found, 48);
-    assert_ge!(items_loaded.reference_items.reference_locations_found, 175);
+    assert_ge!(items_loaded.base_plants_found, 194);
+    assert_ge!(items_loaded.base_types_found, 49);
+    assert_ge!(items_loaded.reference_items.reference_locations_found, 188);
     assert_ge!(
         items_loaded.reference_items.reference_base_plants_added,
-        2724
+        2948
     );
-    assert_ge!(items_loaded.reference_items.reference_plants_added, 5042);
+    assert_ge!(items_loaded.reference_items.reference_plants_added, 5346);
 }
 
 #[test]
