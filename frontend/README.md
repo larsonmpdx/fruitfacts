4d8f1ebfc5397e549622b8a4cd681fdbae283c2a
larsonmpdx <larsonm.pdx@gmail.com>
2022-01-31 20:59:37 -0800

    more

--- frontend/README.md
+++ frontend/README.md
@@ -1,11 +1,11 @@
 # js hints
 
-- install nvm (there is a related windows project)
-  - `nvm install lts` and `nvm use lts`
-- `npm run dev/build/start` next.js things
-- `ncu -u` update package.json versions (after installing `npm i -g npm-check-updates`)
+-   install nvm (there is a related windows project)
+    -   `nvm install lts` and `nvm use lts`
+-   `npm run dev/build/start` next.js things
+-   `ncu -u` update package.json versions (after installing `npm i -g npm-check-updates`)
 
 # issues I'm tracking
 
-- Jan 2022: not enough options for redirects so we can't have nested paths with some paths ending in '/' and some not
-  - https://github.com/vercel/next.js/discussions/23988
+-   Jan 2022: not enough options for redirects so we can't have nested paths with some paths ending in '/' and some not
+    -   https://github.com/vercel/next.js/discussions/23988
--- frontend/pages/plant/[type]/[name].js
+++ frontend/pages/plant/[type]/[name].js
@@ -1,88 +1,94 @@
 export async function getServerSideProps(context) {
-	const { type, name } = context.query;
-	const plant = await fetch(`${process.env.BACKEND_BASE}/api/plants/${type}/${name}`)
-		.then((response) => {
-			if (response.status !== 200) {
-				return {};
-			}
-			return response.json();
-		})
-		.catch((error) => {
-			console.log(error);
-			return {};
-		});
+    const { type, name } = context.query;
+    const plant = await fetch(`${process.env.BACKEND_BASE}/api/plants/${type}/${name}`)
+        .then((response) => {
+            if (response.status !== 200) {
+                return {};
+            }
+            return response.json();
+        })
+        .catch((error) => {
+            console.log(error);
+            return {};
+        });
 
-	return {
-		props: {
-			plant
-		}
-	};
+    return {
+        props: {
+            plant
+        }
+    };
 }
 
 export default function Home({ plant }) {
-	return (
-		<div>
-			{plant.base && (
-				<h2>
-					{plant.base.name} {plant.base.type}
-				</h2>
-			)}
-			{plant.base?.marketing_name && <h2>marketed as {plant.base.marketing_name} </h2>}
+    return (
+        <div>
+            {plant.base && (
+                <h2>
+                    {plant.base.name} {plant.base.type}
+                </h2>
+            )}
+            {plant.base?.marketing_name && <h2>marketed as {plant.base.marketing_name} </h2>}
 
-			{plant.base?.uspp_number && <p>USPP {plant.base.uspp_number}</p>}
+            {plant.base?.uspp_number && <p>USPP {plant.base.uspp_number}</p>}
 
-			{plant.base?.uspp_expiration && <p>expires {plant.base.uspp_expiration}</p>}
+            {plant.base?.uspp_expiration && <p>expires {plant.base.uspp_expiration}</p>}
 
-			{plant.base?.aka && <p>AKA {plant.base.aka}</p>}
+            {plant.base?.aka && <p>AKA {plant.base.aka}</p>}
 
-			{plant.base?.release_year && plant.base?.released_by && (
-				<p>
-					{plant.base?.release_year && <p>{plant.base.release_year}</p>}
-					{plant.base?.released_by && <p>{plant.base.released_by}</p>}
-				</p>
-			)}
+            {plant.base?.release_year && plant.base?.released_by && (
+                <p>
+                    {plant.base?.release_year && <p>{plant.base.release_year}</p>}
+                    {plant.base?.released_by && <p>{plant.base.released_by}</p>}
+                </p>
+            )}
 
-			{plant.collection && (
-				<>
-					{' '}
-					<h1>Collections</h1>
-					<ul>
-						{plant.collection.map((entry) => (
-							<>
-								<li>
-									<a href={`/collections/${encodeURIComponent(entry.path_and_filename)}`}>
-										{entry.path_and_filename}
-									</a>
-									{entry.description && <p>{entry.description}</p>}
-								</li>
-							</>
-						))}
-					</ul>
-				</>
-			)}
+            {plant.collection && (
+                <>
+                    {' '}
+                    <h1>Collections</h1>
+                    <ul>
+                        {plant.collection.map((entry) => (
+                            <>
+                                <li>
+                                    <a
+                                        href={`/collections/${encodeURIComponent(
+                                            entry.path_and_filename
+                                        )}`}
+                                    >
+                                        {entry.path_and_filename}
+                                    </a>
+                                    {entry.description && <p>{entry.description}</p>}
+                                </li>
+                            </>
+                        ))}
+                    </ul>
+                </>
+            )}
 
-			{plant.collection && (
-				<>
-					<h1>Harvest Times</h1>
-					<ul>
-						{plant.collection.map((entry) => (
-							<>
-								{entry.harvest_text && (
-									<p>
-										{entry.harvest_text}
-										<a
-											href={`/collections/${encodeURIComponent(entry.path_and_filename)}`}
-											title={entry.path_and_filename}
-										>
-											[ref]
-										</a>
-									</p>
-								)}
-							</>
-						))}
-					</ul>
-				</>
-			)}
-		</div>
-	);
+            {plant.collection && (
+                <>
+                    <h1>Harvest Times</h1>
+                    <ul>
+                        {plant.collection.map((entry) => (
+                            <>
+                                {entry.harvest_text && (
+                                    <p>
+                                        {entry.harvest_text}
+                                        <a
+                                            href={`/collections/${encodeURIComponent(
+                                                entry.path_and_filename
+                                            )}`}
+                                            title={entry.path_and_filename}
+                                        >
+                                            [ref]
+                                        </a>
+                                    </p>
+                                )}
+                            </>
+                        ))}
+                    </ul>
+                </>
+            )}
+        </div>
+    );
 }
