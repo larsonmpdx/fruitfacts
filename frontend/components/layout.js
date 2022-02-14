481ee49db0c4b3ad47050d6c77f29492aeefa71b
larsonmpdx <larsonm.pdx@gmail.com>
2022-02-14 14:43:33 -0800

    Revert "bulma initial"
    This reverts commit 38405ee979040dd5e5a455817974ccd60317a5af.

--- frontend/components/layout.js
+++ frontend/components/layout.js
@@ -3,7 +3,6 @@ import React from 'react';
 import { useRouter } from 'next/router';
 import Search from './navbarSearch';
 import Login from './navbarLogin';
-import Navbar from './navbar';
 
 export default function Layout({ children }) {
     const [user, setUser] = React.useState({});
@@ -15,7 +14,6 @@ export default function Layout({ children }) {
                 <noscript>
                     <p>{process.env.NEXT_PUBLIC_SITE_NAME} works better with javascript</p>
                 </noscript>
-                <Navbar />
                 <Link href="/">Fruitfacts</Link>
                 <Search />
                 <Login user={user} setUser={setUser} />
--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,4 +1,3 @@
-import 'bulma/css/bulma.min.css';
 import Layout from '../components/layout';
 
 export default function MyApp({ Component, pageProps }) {
