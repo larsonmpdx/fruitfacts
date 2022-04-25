93acbfdb17e9b4061935472224fa39b3e93ab5c2
larsonmpdx <larsonm.pdx@gmail.com>
2022-04-24 21:55:46 -0700

    more

--- server_admin/get_ubuntu_dependencies.sh
+++ server_admin/get_ubuntu_dependencies.sh
@@ -3,7 +3,18 @@ set -e
 
 sudo apt update
 sudo apt upgrade -y
-sudo apt install -y build-essential pkg-config libssl-dev
+
+# some of these might be able to be trimmed now that we're using llvm
+sudo apt install -y build-essential pkg-config libssl-dev software-properties-common
+
+# install llvm so we can link with lld
+# it takes less memory so it can be done right on the server
+# https://apt.llvm.org/
+wget -O - https://apt.llvm.org/llvm-snapshot.gpg.key|sudo apt-key add
+sudo add-apt-repository 'deb http://apt.llvm.org/focal/ llvm-toolchain-focal-14 main' # focal is ubuntu 20.04 (lts)
+sudo apt-get -y install clang-14 lldb-14 lld-14 # llvm 14 released march 2022
+ln -s /usr/bin/clang-14 /usr/bin/clang
+ln -s /usr/bin/lld-14 /usr/bin/lld
 
 # https://forge.rust-lang.org/infra/other-installation-methods.html#rustup
 curl https://sh.rustup.rs -sSf | sh
