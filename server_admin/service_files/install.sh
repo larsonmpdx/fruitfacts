dd3ac80ebfd7d4d4789621a6b64334fb82c6b303
larsonmpdx <larsonm.pdx@gmail.com>
2022-04-25 10:12:07 -0700

    a

--- server_admin/service_files/install.sh
+++ server_admin/service_files/install.sh
@@ -3,6 +3,8 @@ set -e
 
 cp ./*.service /etc/systemd/system/
 systemctl daemon-reload
+systemctl enable frontend_fruitfacts.service
+systemctl enable backend_fruitfacts.service
 
 # run like:
 # sudo systemctl start fruitfacts_frontend.service
--- server_admin/set_folder_permissions_for_www-data.sh
+++ server_admin/set_folder_permissions_for_www-data.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 set -e
 
+git config --global --add safe.directory /var/www/fruitfacts
+
 cd ..
 sudo chown -R www-data:www-data .
 cd -
