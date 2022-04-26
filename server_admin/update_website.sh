ecda8afe8cbe2a4094f0d963d524cf7cee1e5ed9
larsonmpdx <larsonm.pdx@gmail.com>
2022-04-25 19:40:42 -0700

    Update update_website.sh

--- server_admin/update_website.sh
+++ server_admin/update_website.sh
@@ -9,5 +9,5 @@ npm run build
 cd ../backend/
 cargo build --release
 systemctl stop backend_fruitfacts.service || true
-cargo run -- -reload_db
+cargo run -- --reload_db
 systemctl start backend_fruitfacts.service
