32fda7d87b2a996a4dee8da272932ce88886b169
larsonmpdx <larsonm.pdx@gmail.com>
2022-04-27 08:42:40 -0700

    fix

--- server_admin/letsencrypt/1_get_cert.sh
+++ server_admin/letsencrypt/1_get_cert.sh
@@ -2,13 +2,10 @@
 set -e
 cd "$(dirname "${BASH_SOURCE[0]}")"
 
-# digitalocean credentials file notes (needs to be manually created)
-# https://certbot-dns-digitalocean.readthedocs.io/en/stable/
-
 # first create an ini file:
 # https://www.digitalocean.com/community/tutorials/how-to-acquire-a-let-s-encrypt-certificate-using-dns-validation-with-certbot-dns-digitalocean-on-ubuntu-20-04
 # -> save to ~/digitalocean.ini
-chmod go-rwx ~/digitalocean.ini
+chmod go-rwx ~/digitalocean.ini # certbot complains if access isn't limited
 
 # then run this
 certbot certonly \
--- server_admin/letsencrypt/2_certbot_add_ssl_to_nginx_files.sh
+++ server_admin/letsencrypt/2_certbot_add_ssl_to_nginx_files.sh
@@ -2,6 +2,7 @@
 set -e
 cd "$(dirname "${BASH_SOURCE[0]}")"
 
+# this command has certbot go through our file in /etc/nginx/sites-enabled/ and add managed ssl bits to it
 sudo certbot --nginx -d fruitfacts.xyz -d www.fruitfacts.xyz -d api.fruitfacts.xyz
 
 # ... there will be interactive stuff to do here
