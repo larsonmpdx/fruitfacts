9d7a706dd2aae753867566a51b45445b8e849823
larsonmpdx <larsonm.pdx@gmail.com>
2022-04-27 07:50:22 -0700

    a

--- server_admin/letsencrypt/0_copy_nginx_base_files.sh
+++ server_admin/letsencrypt/0_copy_nginx_base_files.sh
@@ -5,6 +5,6 @@ cd "$(dirname "${BASH_SOURCE[0]}")"
 
 # these are base files that will have ssl-stuff added to them by certbot
 cp ./nginx_base_files/fruitfacts.conf /etc/nginx/sites-available/
-ln -s /etc/nginx/sites-available/fruitfacts.conf /etc/nginx/sites-enabled/fruitfacts.conf
+ln -s /etc/nginx/sites-available/fruitfacts.conf /etc/nginx/sites-enabled/fruitfacts.conf || true
 
 sudo service nginx reload
