1544c59cbba4182242225642f85588185a3cfdac
larsonmpdx <larsonm.pdx@gmail.com>
2022-05-01 21:18:25 -0700

    show full user

--- frontend/pages/user.js
+++ frontend/pages/user.js
@@ -5,5 +5,33 @@ export default function Home({ user, setContributingLinks }) {
     setContributingLinks([{ link: `/frontend/pages/user.js`, description: `user.js` }]);
   }, []);
 
-  return <>{user && <p>{JSON.stringify(user)}</p>}</>;
+  const [fullUser, setFullUser] = React.useState(null);
+
+  React.useEffect(() => {
+    // todo - this is too many lines for what it does. simplify (and other occurrences)
+    const fetchData = async () => {
+      const data = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_BASE}/api/getFullUser`, {
+        credentials: 'include'
+      })
+        .then((response) => {
+          if (response.status === 200) {
+            return response.json();
+          } else {
+            setErrorMessage("couldn't get full user");
+            return null;
+          }
+        })
+        .catch((error) => {
+          setErrorMessage(`couldn't get full user: ${error.message}`);
+          console.log(error);
+          return null;
+        });
+
+        setFullUser(data);
+    };
+
+    fetchData();
+  }, []);
+
+  return <>{fullUser && <p>{JSON.stringify(fullUser)}</p>}</>;
 }
