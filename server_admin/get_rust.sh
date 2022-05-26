2bad21523a6bda2ecab7ca14ddbc0779af281abb
larsonmpdx <larsonm.pdx@gmail.com>
2022-05-25 20:48:57 -0700

    working on DWN harvest charts

--- server_admin/get_rust.sh
+++ server_admin/get_rust.sh
@@ -4,7 +4,7 @@ cd "$(dirname "${BASH_SOURCE[0]}")"
 
 # use this method so it's installed for all users (rustup is only for one user)
 # https://forge.rust-lang.org/infra/other-installation-methods.html#standalone-installers
-# this *doesn't* include updates. todo, handle that somehow
+rm ./*.tar.gz
 wget https://static.rust-lang.org/dist/rust-1.61.0-x86_64-unknown-linux-gnu.tar.gz
 tar -xf ./rust*.tar.gz
 cd ./rust*/
