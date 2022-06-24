8dbca322475ee56329708f27334ab856c916f2c3
larsonmpdx <larsonm.pdx@gmail.com>
2022-06-24 10:20:52 -1000

    search update

--- backend/src/queries/search.rs
+++ backend/src/queries/search.rs
@@ -8,10 +8,12 @@ type DbPool = r2d2::Pool<ConnectionManager<SqliteConnection>>;
 use regex::Regex;
 use serde::{Deserialize, Serialize};
 
+use anyhow::{anyhow, Result};
+
 #[derive(Deserialize)]
-struct SearchQuery {
-    searchType: Option<String>, // base plants or collection items. todo: user items, "search all"
-    search: Option<String>,     // search string like "PF 11"
+pub struct SearchQuery {
+    search_type: Option<String>, // base plants or collection items. todo: user items, "search all"
+    search: Option<String>,      // search string like "PF 11"
     name: Option<String>, // exact plant name (allows getting a single base plant). probably doesn't make sense when used with search
     patents: Option<bool>,
     #[serde(rename = "type")]
@@ -58,9 +60,8 @@ struct SearchQuery {
 
 #[derive(Default, Queryable, Serialize)]
 pub struct SearchReturn {
-    #[serde(rename = "type")]
-    type_: Option<String>, // base plants or collection
-    count: Option<i32>, // total count of search results (if paginated)
+    search_type: Option<String>, // base plants or collection
+    count: Option<i32>,          // total count of search results (if paginated)
     page: Option<i32>,
     patent_midpoint_page: Option<i32>, // special case: if we did a patent search, which page has the transition from past to future expirations?
     pub base_plants: Option<Vec<BasePlant>>,
@@ -73,127 +74,138 @@ pub struct SearchReturn {
 
 // todo: I want to bring all of the search & filter queries into one API
 
-pub fn variety_search_db(
-    db_conn: &SqliteConnection,
-    input: &str,
-) -> Result<Vec<BasePlant>, diesel::result::Error> {
-    // remove extra characters. leave spaces so we can treat separate words as separate
-    // dashes get interpreted by fts. same with +*:^ AND OR NOT
-    let re = Regex::new(r"[^0-9A-Za-z ]").unwrap();
-    let cleaned = re.replace_all(input.trim(), ""); // also trim() for leading/trailing whitespace
+pub fn search_db(db_conn: &SqliteConnection, query: &SearchQuery) -> Result<SearchReturn> {
+    if (query.search_type.is_none()) {
+        return Err(anyhow!("search_type not set"));
+    }
 
-    // replace multiple spaces with a single space
-    let re = Regex::new(r"\s+").unwrap();
-    let multiple_spaces_removed = re.replace_all(&cleaned, " ");
-
-    let mut statement = multiple_spaces_removed.split(' ').collect::<Vec<&str>>();
-
-    // if we have multiple search words, also add a search element which is all of them concatenated
-    // allows searching for "pf 11" which would otherwise be two chars
-    let split_for_count = multiple_spaces_removed.split(' ').collect::<Vec<&str>>();
-
-    // look at the last element to see if one of our types starts with this - if so we'll restrict results to this type
-    let mut restrict_to_type: Option<String> = None;
-    if split_for_count.len() >= 2 {
-        for type_ in crate::import_db::types_generated::TYPES.iter() {
-            if *type_.to_lowercase() == split_for_count.last().unwrap().to_lowercase() {
-                restrict_to_type = Some(type_.to_string());
-                break;
+    match query.search_type.as_ref().unwrap().as_str() {
+        "base" => {
+            let search_results;
+            if (query.search.is_none()) {
+                search_results = None;
             }
-        }
-    }
+            else {
+                let input = query.search.as_ref().unwrap();
+                // remove extra characters. leave spaces so we can treat separate words as separate
+                // dashes get interpreted by fts. same with +*:^ AND OR NOT
+                let re = Regex::new(r"[^0-9A-Za-z ]").unwrap();
+                let cleaned = re.replace_all(input.trim(), ""); // also trim() for leading/trailing whitespace
 
-    if restrict_to_type.is_some() {
-        statement.pop(); // we got a type by matching on the last search element, remove it from the FTS search words
-    }
+                // replace multiple spaces with a single space
+                let re = Regex::new(r"\s+").unwrap();
+                let multiple_spaces_removed = re.replace_all(&cleaned, " ");
 
-    let mut statement_string = statement
-        .clone()
-        .into_iter()
-        .map(|x| format!("\"{x}\"")) // double quote each element - allows searching for special characters or keywords like "OR"
-        .collect::<Vec<String>>()
-        .join(" OR ");
+                let mut statement = multiple_spaces_removed.split(' ').collect::<Vec<&str>>();
 
-    // if we have multiple words, try adding one last search term which is all of them concatenated
-    // this helps us with "pf 1" for example
-    if split_for_count.len() >= 2 {
-        statement_string.push_str(&format!(" OR \"{}\"", statement.join("")));
-    }
+                // if we have multiple search words, also add a search element which is all of them concatenated
+                // allows searching for "pf 11" which would otherwise be two chars
+                let split_for_count = multiple_spaces_removed.split(' ').collect::<Vec<&str>>();
 
-    println!("input {input} cleaned: {cleaned} ORed: {statement_string}");
-
-    const N_MAX: i64 = 10;
-
-    let mut query = fts_base_plants::table
-        .select((fts_base_plants::rowid, fts_base_plants::rank))
-        .filter(fts_base_plants::whole_row.eq(statement_string))
-        .order(fts_base_plants::rank.asc())
-        .into_boxed();
-
-    match restrict_to_type {
-        None => {
-            query = query.limit(N_MAX);
-        }
-        _ => {
-            query = query.limit(N_MAX * 10); // we gather more if we're going to later restrict by type
-        }
-    }
-
-    let values = query.load::<FtsBasePlants>(db_conn);
-    // todo - maybe limit 100 or something? we want to get a bunch though in case we're limiting to only one variety later
-    // todo - report total search results if limiting to N
-
-    println!("{:?}", values);
-
-    // todo: order or limit by notoriety
-    match values {
-        Ok(values) => {
-            let ids_nullable: Vec<_> = values.iter().map(|x| x.rowid).collect();
-
-            // step through IDs and get the original row
-            // this lets us preserve FTS ranking
-            let mut results: Vec<BasePlant> = Default::default();
-
-            match restrict_to_type {
-                None => {
-                    for id in &ids_nullable {
-                        let result = base_plants::dsl::base_plants
-                            .filter(base_plants::id.eq(id))
-                            .first::<BasePlant>(db_conn)
-                            .unwrap();
-
-                        results.push(result);
-                    }
-                }
-
-                Some(type_) => {
-                    for id in &ids_nullable {
-                        let result = base_plants::dsl::base_plants
-                            .filter(base_plants::id.eq(id))
-                            .filter(base_plants::type_.eq(&type_))
-                            .first::<BasePlant>(db_conn); // this row may or may not match our type filter
-
-                        if let Ok(result) = result {
-                            results.push(result);
-                            if results.len() as i64 >= N_MAX {
-                                break; // we limit here because our fts search was allowed to be for more results before we did our type filter
-                            }
+                // look at the last element to see if one of our types starts with this - if so we'll restrict results to this type
+                let mut restrict_to_type: Option<String> = None;
+                if split_for_count.len() >= 2 {
+                    for type_ in crate::import_db::types_generated::TYPES.iter() {
+                        if *type_.to_lowercase() == split_for_count.last().unwrap().to_lowercase() {
+                            restrict_to_type = Some(type_.to_string());
+                            break;
                         }
                     }
                 }
-            };
 
-            println!("{:?}", results);
+                if restrict_to_type.is_some() {
+                    statement.pop(); // we got a type by matching on the last search element, remove it from the FTS search words
+                }
 
-            Ok(results)
+                let mut statement_string = statement
+                    .clone()
+                    .into_iter()
+                    .map(|x| format!("\"{x}\"")) // double quote each element - allows searching for special characters or keywords like "OR"
+                    .collect::<Vec<String>>()
+                    .join(" OR ");
+
+                // if we have multiple words, try adding one last search term which is all of them concatenated
+                // this helps us with "pf 1" for example
+                if split_for_count.len() >= 2 {
+                    statement_string.push_str(&format!(" OR \"{}\"", statement.join("")));
+                }
+
+                println!("input {input} cleaned: {cleaned} ORed: {statement_string}");
+
+                const N_MAX: i64 = 10;
+
+                let mut query = fts_base_plants::table
+                    .select((fts_base_plants::rowid, fts_base_plants::rank))
+                    .filter(fts_base_plants::whole_row.eq(statement_string))
+                    .order(fts_base_plants::rank.asc())
+                    .into_boxed();
+
+                match restrict_to_type {
+                    None => {
+                        query = query.limit(N_MAX);
+                    }
+                    _ => {
+                        query = query.limit(N_MAX * 10); // we gather more if we're going to later restrict by type
+                    }
+                }
+
+                let values = query.load::<FtsBasePlants>(db_conn);
+                // todo - maybe limit 100 or something? we want to get a bunch though in case we're limiting to only one variety later
+                // todo - report total search results if limiting to N
+
+                println!("{:?}", values);
+
+                // todo: order or limit by notoriety
+                search_results = match values {
+                    Ok(values) => {
+                        let ids_nullable: Vec<_> = values.iter().map(|x| x.rowid).collect();
+
+                        // step through IDs and get the original row
+                        // this lets us preserve FTS ranking
+                        let mut results: Vec<BasePlant> = Default::default();
+
+                        match restrict_to_type {
+                            None => {
+                                for id in &ids_nullable {
+                                    let result = base_plants::dsl::base_plants
+                                        .filter(base_plants::id.eq(id))
+                                        .first::<BasePlant>(db_conn)
+                                        .unwrap();
+
+                                    results.push(result);
+                                }
+                            }
+
+                            Some(type_) => {
+                                for id in &ids_nullable {
+                                    let result = base_plants::dsl::base_plants
+                                        .filter(base_plants::id.eq(id))
+                                        .filter(base_plants::type_.eq(&type_))
+                                        .first::<BasePlant>(db_conn); // this row may or may not match our type filter
+
+                                    if let Ok(result) = result {
+                                        results.push(result);
+                                        if results.len() as i64 >= N_MAX {
+                                            break; // we limit here because our fts search was allowed to be for more results before we did our type filter
+                                        }
+                                    }
+                                }
+                            }
+                        };
+
+                        println!("{:?}", results);
+
+                        Some(results)
+                    }
+                    Err(error) => None,
+                }
+            }
+
+            Err(anyhow!(""))
         }
-        Err(error) => Err(error),
+        "coll" => Err(anyhow!("")),
+        _ => Err(anyhow!("")),
     }
-}
-
-#[derive(Deserialize)]
-struct SearchPath {
-    string: String,
 }
 
 // search test cases:
@@ -211,14 +223,14 @@ struct SearchPath {
 
 // todo: if we have an exact match for a type name (or type aka name) then remove that word, use it to suggest that type
 // todo - this kind of type search plus a full text search on the collections json files
-#[get("/api/search/{string}")]
+#[get("/api/search")]
 async fn variety_search(
-    path: web::Path<SearchPath>,
+    query: web::Query<SearchQuery>,
     pool: web::Data<DbPool>,
 ) -> Result<HttpResponse, actix_web::Error> {
     let conn = pool.get().expect("couldn't get db connection from pool");
 
-    let results = web::block(move || variety_search_db(&conn, &path.string))
+    let results = web::block(move || search_db(&conn, &query))
         .await
         .unwrap(); // todo - blockingerror unwrap?
 
