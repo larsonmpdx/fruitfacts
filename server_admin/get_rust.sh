f895f2afd95565028d5202ad2c27b08d82f81c09
larsonmpdx <larsonm.pdx@gmail.com>
2022-07-05 20:34:27 -0700

    fix

--- server_admin/get_rust.sh
+++ server_admin/get_rust.sh
@@ -4,6 +4,7 @@ cd "$(dirname "${BASH_SOURCE[0]}")"
 
 # use this method so it's installed for all users (rustup is only for one user)
 # https://forge.rust-lang.org/infra/other-installation-methods.html#standalone-installers
+rm -rf ./rust*/
 rm ./*.tar.gz
 wget https://static.rust-lang.org/dist/rust-1.62.0-x86_64-unknown-linux-gnu.tar.gz
 tar -xf ./rust*.tar.gz
