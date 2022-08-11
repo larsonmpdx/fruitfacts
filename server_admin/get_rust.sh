921193e363d803edeed816401c4ee36b3b133ec7
larsonmpdx <larsonm.pdx@gmail.com>
2022-08-11 13:20:46 -0700

    bump

--- server_admin/get_rust.sh
+++ server_admin/get_rust.sh
@@ -6,7 +6,7 @@ cd "$(dirname "${BASH_SOURCE[0]}")"
 # https://forge.rust-lang.org/infra/other-installation-methods.html#standalone-installers
 rm -rf ./rust*/
 rm ./*.tar.gz
-wget https://static.rust-lang.org/dist/rust-1.62.1-x86_64-unknown-linux-gnu.tar.gz
+wget https://static.rust-lang.org/dist/rust-1.63.0-x86_64-unknown-linux-gnu.tar.gz
 tar -xf ./rust*.tar.gz
 cd ./rust*/
 sudo ./install.sh
