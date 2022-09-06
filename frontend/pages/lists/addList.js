3edc82099a3eed18a6b50857dfbccbdef6125285
larsonmpdx <larsonm.pdx@gmail.com>
2022-09-05 21:02:39 -0700

    simplify list create front end. todo make request work and redirects based on result

--- frontend/pages/lists/addList.js
+++ frontend/pages/lists/addList.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Button from '../../components/button';
 
-export default function Home({ setContributingLinks }) {
+export default function Home({ setErrorMessage, setContributingLinks }) {
   React.useEffect(() => {
     setContributingLinks([
       { link: `/frontend/pages/lists/addList.js`, description: `list.js` },
@@ -9,36 +9,52 @@ export default function Home({ setContributingLinks }) {
     ]);
   }, []);
 
-  const [zipEnabled, setZipEnabled] = React.useState('');
   const [name, setName] = React.useState('');
-  const [zip, setZip] = React.useState('');
-  const [latLon, setLatLon] = React.useState('');
-
-  const handleEnterAsZip = () => {
-    setZipEnabled(true);
-    return; // todo
-  };
+  const [lat, setLat] = React.useState('');
+  const [lon, setLon] = React.useState('');
 
   const handleRequestLocation = () => {
-    setZipEnabled(false);
     navigator.geolocation.getCurrentPosition(function (position) {
       console.log('Latitude is :', position.coords.latitude);
       console.log('Longitude is :', position.coords.longitude);
-      setLatLon(`${position.coords.latitude},${position.coords.longitude}`);
+      setLat(position.coords.latitude);
+      setLon(position.coords.longitude);
     });
 
     return; // todo
   };
 
-  const handleSubmit = () => {
-    return; // todo
+  const handleSubmit = async () => {
+    const data = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_BASE}/api/list`,
+    {
+      method: 'POST',
+      credentials: 'include',
+      body: JSON.stringify({
+        location_name: name,
+        latitude: lat,
+        longitude: lon,
+        user_id: "todo",
+      })
+    })
+    .then((response) => {
+      if (response.status !== 200) {
+        setErrorMessage("can't reach the backend");
+        return [];
+      }
+      return response.json();
+    })
+    .catch((error) => {
+      setErrorMessage(`can't reach backend: ${error.message}`);
+      console.log(error);
+      return [];
+    });
   };
 
   // todo:
   // support both new list creation and editing
   // submit box only goes active when fields are ready. maybe with hints?
   // list name (text box)
-  // location (get user location, or pick on a map, or zip code)
+  // location (get user location, or pick on a map, or (todo) zip code)
 
   // todo - notoriety score is currently not null, switch that to optional
   // initial design - name only
@@ -51,26 +67,15 @@ export default function Home({ setContributingLinks }) {
           Name:
           <input type="text" value={name} onChange={(event) => setName(event.target.value)} />
         </label>
-        {zipEnabled ? (
           <label>
-            zip code:
-            <input type="text" value={zip} onChange={(event) => setZip(event.target.value)} />
+            lat:
+            <input type="text" value={lat} onChange={(event) => setLat(event.target.value)} />
           </label>
-        ) : (
           <label>
-            lat,lon:
-            <input type="text" value={latLon} onChange={(event) => setLatLon(event.target.value)} />
-          </label>
-        )}
+          lon:
+          <input type="text" value={lon} onChange={(event) => setLon(event.target.value)} />
+        </label>
       </form>
-      <Button
-        enabled={true}
-        onClick={async () => {
-          await handleEnterAsZip();
-        }}
-        className="focus:shadow-outline h-12 w-full rounded-lg bg-indigo-700 px-6 text-indigo-100 transition-colors duration-150 hover:bg-indigo-800"
-        label="use a zip code for location"
-      />
       <Button
         enabled={true}
         onClick={async () => {
