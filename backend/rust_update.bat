14096854d59c66648a18e1ab36a3a3fe2c2e54a6
larsonmpdx <larsonm.pdx@gmail.com>
2022-11-03 10:46:48 -0700

    version update

---
+++ backend/rust_update.bat
@@ -0,0 +1,11 @@
+rem run after a rust version update to fetch it and update all tools
+rustup update stable || goto :error
+rustup toolchain install nightly || goto :error
+cargo install --locked cargo-outdated || goto :error
+cargo install cargo-udeps --locked || goto :error
+cargo test -- --include-ignored || goto :error
+goto :EOF
+
+:error
+echo Failed with error #%errorlevel%.
+exit /b %errorlevel%
