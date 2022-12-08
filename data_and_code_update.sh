c8ec4e214004e2533adef39d2fd8b2e88f197916
larsonmpdx <larsonm.pdx@gmail.com>
2022-12-07 23:13:40 -0800

    stop services because we need a little more ram for the backend build

--- data_and_code_update.sh
+++ data_and_code_update.sh
@@ -5,5 +5,12 @@ cd "$(dirname "${BASH_SOURCE[0]}")"
 git checkout .
 ./server_admin/set_folder_permissions_for_www-data.sh
 sudo -u www-data git pull
+
+service backend_fruitfacts stop
+service frontend_fruitfacts stop
+
 ./server_admin/code_update.sh
 ./server_admin/database_update.sh
+
+service backend_fruitfacts start
+service frontend_fruitfacts start
