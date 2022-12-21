86f33bb6a57243b9b21175ded2e0554ded621049
larsonmpdx <larsonm.pdx@gmail.com>
2022-12-20 20:46:12 -0800

    Update next.config.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,3 +1,7 @@
 module.exports = {
-  // empty  - was messing with this for openlayers which I removed
-};
+  webpack5: true,
+  webpack: config => {
+    config.resolve.fallback = { fs: false, path: false } // fixes "Module not found: Can't resolve 'fs'" see https://stackoverflow.com/questions/64926174/module-not-found-cant-resolve-fs-in-next-js-application
+    return config
+  }
+}
