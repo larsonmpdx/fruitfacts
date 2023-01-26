b7188928998641abdcb1ad5e8f8b93aec1633594
larsonmpdx <larsonm.pdx@gmail.com>
2023-01-25 18:16:19 -0800

    bump

---
+++ frontend/node_update.bat
@@ -0,0 +1,19 @@
+rem updates node and globally-installed tools
+
+rem get admin
+Net session >nul 2>&1 || (
+    PowerShell start -verb runas '%~0'
+    pause
+    exit /b
+)
+
+nvm install lts || goto :error
+nvm use lts || goto :error
+npm i -g npm-check-updates || goto :error
+pause
+goto :EOF
+
+:error
+echo Failed with error #%errorlevel%.
+pause
+exit /b %errorlevel%
