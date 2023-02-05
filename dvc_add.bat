89f61b9c8bae3782b464d0ce45128e84a146f8ec
larsonmpdx <larsonm.pdx@gmail.com>
2023-02-05 11:24:49 -0800

    working on a tennessee pub

--- dvc_add.bat
+++ dvc_add.bat
@@ -2,8 +2,10 @@ rem run after a rust version update to fetch it and update all tools
 dvc add --glob -R plant_database\**\*.pdf || goto :error
 dvc add --glob -R frontend\public\data\**\*.jpg || goto :error
 dvc diff || goto :error
+dvc commit || goto :error
+dvc push || goto :error
 
-echo next steps are "dvc commit" then "dvc push" and also adding .dvc files to git
+echo next step is to add .dvc files to git
 
 goto :EOF
 
