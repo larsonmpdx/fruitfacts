f182de8bb660439e3aad0712f6e8359543fe8030
larsonmpdx <larsonm.pdx@gmail.com>
2024-12-25 15:56:14 -0800

    update update script

--- frontend/node_update.bat
+++ frontend/node_update.bat
@@ -1,19 +1,30 @@
 rem updates node and globally-installed tools
 
 rem get admin for "nvm use..."
-Net session >nul 2>&1 || (
-    PowerShell start -verb runas '%~0'
+net file 1>NUL 2>NUL
+if not '%errorlevel%' == '0' (
+    powershell Start-Process -FilePath "%0" -ArgumentList "%cd%" -verb runas >NUL 2>&1
     pause
     exit /b
 )
 
-nvm install lts || goto :error
-nvm use lts || goto :error
-npm i -g npm-check-updates || goto :error
-pause
+:: Change directory with passed argument. Processes started with
+:: "runas" start with forced C:\Windows\System32 workdir
+cd /d %1
+
+call nvm install lts || goto :error
+call nvm use lts || goto :error
+call npm i -g npm-check-updates || goto :error
+
+echo "store versions to a file"
+echo | set /p dummy_name="node: " >node_versions.txt || goto :error
+call nvm current >>node_versions.txt || goto :error
+echo | set /p dummy_name="npm-check-updates: " >>node_versions.txt || goto :error
+call npm-check-updates --version >>node_versions.txt || goto :error
+
+echo "finished"
 goto :EOF
 
 :error
 echo Failed with error #%errorlevel%.
-pause
 exit /b %errorlevel%
